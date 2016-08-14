a3c4c3cd1f640ee6169ac051146e28916bfab443
Drew <deeringa@gmail.com>
2016-08-14 00:25:22 -0500

    removed commented out rendering of static html now that ejs templating is being used.

--- calculator.js
+++ calculator.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var path = require('path');
 
 router.get('/', function (req, res) {
-	//res.sendFile(path.join(__dirname+'/views/html/index.html'));
   res.render('index');
 });
 
